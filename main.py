a8eb88b150828ccd5376e874cda7c2fd207a5ca9
DrinkH20 <89287648+DrinkH20@users.noreply.github.com>
2022-01-19 21:22:08 -0800

    Just renamed a variable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from saved_data import *
 
-user_accts = users
+user_acts = users
 
 user_type = input("Do you have an account with us? y/n ")
 
@@ -8,17 +8,17 @@ if user_type == "y":
     name = input("what is your username?")
     password = input("what is your password")
 
-    for i in user_accts:
+    for i in user_acts:
         if i == (name, password):
             print("your in")
 else:
     user_name = input("What would your like your username to be? ")
     user_password = input("What would you like your password to be? ")
-    user_accts.append((user_name, user_password))
-    print(user_accts[len(user_accts) - 1])
+    user_acts.append((user_name, user_password))
+    print(user_acts[len(user_acts) - 1])
 
 file = open("saved_data.py", "w")
 
-file.write("users = " + str(user_accts) + "\n")
+file.write("users = " + str(user_acts) + "\n")
 
 file.close()
